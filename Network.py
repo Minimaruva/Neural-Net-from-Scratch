58ad4c65babfae3e438e2c2facf4f5a0e2ec721e
Vlad Y <vladjagnion@gmail.com>
2025-07-02 22:56:17 +0100

    more data manipulation and future todos

--- Network.py
+++ Network.py
@@ -1,12 +1,41 @@
 import numpy as np
 import pandas as pd
 
-with open ('./Titanic_data/train.csv', 'r') as file:
-    data = pd.read_csv(file)
 
 
+train_data_path = './Titanic_data/train.csv'
+test_data_path = './Titanic_data/test.csv'
+
+with open (train_data_path, 'r') as train_file, open(test_data_path, 'r') as test_file:
+    train_data = pd.read_csv(train_file)
+    test_data = pd.read_csv(test_file)
+
+
+print(train_data.head())
 # Display column names
-col_names = data.columns.values
+col_names = train_data.columns.values
 print(col_names)
 
 # Immediately drop non-numeric columns
+## TODO: show user non-numeric columns and ask if they want to drop them or encode them
+'''
+Potential solution for encoding non-numeric columns:
+from sklearn.preprocessing import LabelEncoder
+enc = LabelEncoder()
+enc.fit(df['COL1'])
+df['COL1'] = enc.transform(df['col1'])
+'''
+
+train_data = train_data.select_dtypes(include=[np.number])
+test_data = test_data.select_dtypes(include=[np.number])
+
+print(train_data.head())
+
+# Fill missing values with the mean of each column
+## ToDo tell user about missing values and let them choose how to handle them
+print("Missing values in train data:\n", train_data.isnull().sum())
+print("Missing values in test data:\n", test_data.isnull().sum())
+
+train_data = train_data.fillna(train_data.mean())
+test_data = test_data.fillna(test_data.mean())
+
