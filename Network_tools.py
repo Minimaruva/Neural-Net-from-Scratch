64cb98780483cff22171828c58caf5f29e6751da
Vlad Y <vladjagnion@gmail.com>
2025-07-06 22:50:28 +0100

    changed normalisation to return mean and std to use for further evaluations

--- Network_tools.py
+++ Network_tools.py
@@ -6,12 +6,12 @@ hidden_layers = 3  # number of layers(excluding input layer)
 features = 2  # number of features
 outputs = 1  # number of output nodes
 
-def init_network(input_size=features, hidden_size=features+1, output_size=outputs, num_hidden_layers=hidden_layers, alpha=1, beta=1):
+def init_network(input_size, hidden_size=None, labels_size=1, num_hidden_layers=3, alpha=1, beta=1):
     weights = []
     biases = []
-    
-    alpha = 1  # scaling factor for weights
-    beta = 1   # scaling factor for biases
+
+    if hidden_size is None:
+        hidden_size = input_size + 1
 
     # Input to first hidden layer
     weights.append(np.random.randn(hidden_size, input_size) * alpha)
@@ -23,9 +23,26 @@ def init_network(input_size=features, hidden_size=features+1, output_size=output
         biases.append(np.random.randn(hidden_size, 1) * beta)
     
     # Last hidden to output layer
-    weights.append(np.random.randn(output_size, hidden_size) * alpha)
-    biases.append(np.random.randn(output_size, 1) * beta)
+    weights.append(np.random.randn(labels_size, hidden_size) * alpha)
+    biases.append(np.random.randn(labels_size, 1) * beta)
     
     return weights, biases
+
+def sigmoid(arr):
+    return 1 / (1+np.exp(-1*arr))
+
+
+def feed_forward(input_layer, num_hidden_layers, weights, biases):
+  cache = [input_layer]
+  
+  for i in range(0,num_hidden_layers+1):
+    prev_layer = cache[i]
+    W = weights[i]
+    b = biases[i]
+    Z = W @ prev_layer + b
+    A = sigmoid(Z)
+    cache.append(A)
+
+  return cache
 
 
--- data_prep.py
+++ data_prep.py
@@ -2,11 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
-labels=['Survived']
-train_data_path = './Titanic_data/train.csv'
-test_data_path = './Titanic_data/test.csv'
-
-
 def load_data_train_test(train_data_path, test_data_path):
     with open (train_data_path, 'r') as train_file, open(test_data_path, 'r') as test_file:
         train_data = pd.read_csv(train_file)
@@ -15,7 +10,6 @@ def load_data_train_test(train_data_path, test_data_path):
 
     col_names = train_data.columns.values
     return train_data, test_data, col_names
-
 
 
 def encode_non_numeric(df):
@@ -32,12 +26,17 @@ def normalize_data_z_score(df, labels=None):
         features = df.drop(columns=labels)
         labels_df = df[labels] 
 
-        features_norm = (features - features.mean()) / features.std()
+        mean = features.mean()
+        std = features.std()
+
+        features_norm = (features - mean) / std
         df = pd.concat([features_norm, labels_df], axis=1)
     else:
-        df = (df - df.mean()) / df.std()
+        mean = df.mean()
+        std = df.std()
+        df = (df - mean) / std
     
-    return df
+    return df, mean, std
 
 
 def split_reshape_data(df, labels):
@@ -51,39 +50,15 @@ def split_reshape_data(df, labels):
 
     return x_data, y_data
 
-train_data, test_data, col_names = load_data_train_test(train_data_path, test_data_path)
 
-print("Choose columns to drop from the training data:")
 # Display the columns in the training data
-print(train_data.columns)
-
-
-print("Non-numeric columns in training data:", train_data.select_dtypes(exclude=[np.number]).columns.tolist())
-
-train_data = encode_non_numeric(train_data)
-test_data = encode_non_numeric(test_data)
-
-print("After encoding non-numeric columns:")
-print(train_data.head())
-print("*"*100)
 
 
 # Immediately drop non-numeric columns
 ## TODO: show user non-numeric columns and ask if they want to drop them or encode them
 
-
-train_data = train_data.select_dtypes(include=[np.number])
-test_data = test_data.select_dtypes(include=[np.number])
-
-print(train_data.head())
-
 # Fill missing values with the mean of each column
 ## ToDo tell user about missing values and let them choose how to handle them
-print("Missing values in train data:\n", train_data.isnull().sum())
-print("Missing values in test data:\n", test_data.isnull().sum())
-
-train_data = fill_missing_values(train_data)
-test_data = fill_missing_values(test_data)
 
 
 # Normalise the data
@@ -91,46 +66,3 @@ test_data = fill_missing_values(test_data)
 ### https://developers.google.com/machine-learning/crash-course/numerical-data/normalization
 
 
-
-number_of_features = train_data.shape[1] - 1  # Exclude the target variable
-print(f"Number of features: {number_of_features}")
-
-number_of_outputs = 1  # User should specify this based on the target variable
-print(f"Number of outputs: {number_of_outputs}")
-
-print(test_data.head())
-
-train_data = normalize_data_z_score(train_data, labels)
-test_data = normalize_data_z_score(test_data, labels)
-
-x_train, y_train = split_reshape_data(train_data, labels)
-x_test, y_test = split_reshape_data(test_data, labels)
-
-# # Convert DataFrame to numpy arrays
-# x_train = train_data.drop(columns=labels)
-# y_train = train_data[labels]
-
-# x_test = test_data.drop(columns=labels)
-# y_test = test_data[labels]
-
-
-# # Convert the DataFrame to numpy arrays
-# x_train = x_train.to_numpy()
-# y_train = y_train.to_numpy()
-# x_test = x_test.to_numpy()
-# y_test = y_test.to_numpy()
-
-
-# # Reshape the data to ensure it has the correct dimensions
-# train_samples = train_data.shape[0]  # number of samples
-# test_samples = test_data.shape[0]  # number of samples
-
-# # Transpose the input matrix to match the expected shape
-# x_train = x_train.T
-# x_test = x_test.T
-
-# # Reshape outputs to a matrix
-# y_train = y_train.reshape(len(labels), train_samples)
-# y_test = y_test.reshape(len(labels), test_samples)
-
-print(x_train)
